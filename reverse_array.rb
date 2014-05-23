c34bfffe9388035a3b3ef1bf721b15b454ec017d
ahimmelstoss <ahimmelstoss@gmail.com>
2014-05-23 11:00:34 -0400

    solution, fix spec

---
+++ reverse_array.rb
@@ -0,0 +1,11 @@
+def reverse_array(array)
+  first_position = 0
+  last_position = array.length - 1
+  while first_position < last_position
+    array[first_position] = array[last_position]
+    array[last_position] = placeholder
+    first_position += 1
+    last_position -= 1
+  end
+  return array
+end
--- spec/reverse_array_spec.rb
+++ spec/reverse_array_spec.rb
@@ -2,17 +2,17 @@ require_relative 'spec_helper'
 
 describe '#reverse_array' do 
   it 'reverses an array of 5 elements' do
-    let(:my_array) { my_array = ["a", "b", "c", "d", "e"] }
+    my_array = ["a", "b", "c", "d", "e"]
     expect(reverse_array(my_array)).to eq(["e", "d", "c", "b", "a"])
   end
 
   it 'reverses an array of 6 elements' do 
-    let(:my_array) { my_array = ["a", "b", "c", "d", "e", "f"] }
+    my_array = ["a", "b", "c", "d", "e", "f"]
     expect(reverse_array(my_array)).to eq(["f", "e", "d", "c", "b", "a"])
   end
 
   it 'reverses an array of 2 elements' do
-    let(:my_array) { my_array = ["a", "b"] }
+    my_array = ["a", "b"]
     expect(reverse_array(my_array)).to eq(["b", "a"])
   end
 
