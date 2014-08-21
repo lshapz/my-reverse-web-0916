dc1bfea22731322d0049b1268cb766e7827d2e0f
Katie Hoffman <ktahoffman@gmail.com>
2014-08-21 18:16:42 -0400

    pass specs, remove deprecation in spec helper

--- reverse_array.rb
+++ reverse_array.rb
@@ -2,6 +2,7 @@ def reverse_array(array)
   first_position = 0
   last_position = array.length - 1
   while first_position < last_position
+    placeholder = array[first_position]
     array[first_position] = array[last_position]
     array[last_position] = placeholder
     first_position += 1
