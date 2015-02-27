c6bd43f3365e0748278b601c99cb9627982c741e
Katie Hoffman <ktahoffman@gmail.com>
2015-02-26 23:48:23 -0500

    add tests for array class method

--- reverse_array.rb
+++ reverse_array.rb
@@ -1,12 +1,15 @@
-def reverse_array(array)
-  first_position = 0
-  last_position = array.length - 1
-  while first_position < last_position
-    placeholder = array[first_position]
-    array[first_position] = array[last_position]
-    array[last_position] = placeholder
-    first_position += 1
-    last_position -= 1
+class Array
+  def reverse_array
+    array = self
+    first_position = 0
+    last_position = array.length - 1
+    while first_position < last_position
+      placeholder = array[first_position]
+      array[first_position] = array[last_position]
+      array[last_position] = placeholder
+      first_position += 1
+      last_position -= 1
+    end
+    return array
   end
-  return array
 end
