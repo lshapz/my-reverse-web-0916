f50585d5de4da4042ea24d6141141eb70bad4f82
lshapz <laurashapiro01@gmail.com>
2016-12-19 14:58:23 -0500

    Done.

--- reverse_array.rb
+++ reverse_array.rb
@@ -1,1 +1,16 @@
 # your code here
+class Array
+def my_reverse
+  array = self
+  beger = 0
+  ender = array.length - 1
+  while beger < ender 
+    now = array[beger]
+    array[beger] = array[ender]
+    array[ender] = now
+    beger += 1
+    ender -= 1 
+  end
+  array
+end
+end
